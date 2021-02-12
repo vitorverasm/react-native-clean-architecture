c4532f1ea806f34f8d3549edf4c2139e0160d842
Vitor Veras <vitorverasm@gmail.com>
2021-02-12 02:41:15 -0300

    test: ensure RemoteAddAccount calls HttpPostClient with correct URL

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,11 +1,21 @@
 import {AccountModel} from '@/domain/models';
-import {AuthenticationParams} from '@/domain/usecases';
+import {AddAccountParams, AuthenticationParams} from '@/domain/usecases';
 import faker from 'faker';
 
 export const mockAuthentication = (): AuthenticationParams => ({
   email: faker.internet.email(),
   password: faker.internet.password(),
 });
+
+export const mockAddAccount = (): AddAccountParams => {
+  const password = faker.internet.password();
+  return {
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password,
+    passwordConfirmation: password,
+  };
+};
 
 export const mockAccountModel = (): AccountModel => ({
   accessToken: faker.random.uuid(),
