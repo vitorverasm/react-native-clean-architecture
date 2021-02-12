c1d68265c56e34d8cca515596e9ec3f2504ab5f3
Vitor Veras <vitorverasm@gmail.com>
2021-02-12 03:07:52 -0300

    test: ensure RemoteAddAccount throws UnexpectedError if HttpPostClient returns 500

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -53,4 +53,13 @@ describe('RemoteAddAccount', () => {
     const promise = sut.add(mockAddAccount());
     await expect(promise).rejects.toThrow(new UnexpectedError());
   });
+
+  test('Should throw UnexpectedError if HttpPostClient returns 500', () => {
+    const {sut, httpPostClientSpy} = makeSut();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.serverError,
+    };
+    const promise = sut.add(mockAddAccount());
+    expect(promise).rejects.toThrow(new UnexpectedError());
+  });
 });
