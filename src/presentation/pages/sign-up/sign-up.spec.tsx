a468f1de7eb919ede893837b1736bdb453030082
Vitor Veras <vitorverasm@gmail.com>
2021-02-16 16:59:37 -0300

    test: ensure signup page start with initial state and password fields function

--- src/presentation/pages/sign-up/sign-up.spec.tsx
+++ src/presentation/pages/sign-up/sign-up.spec.tsx
@@ -1,12 +1,17 @@
-import {ApplicationProviderMock} from '@/presentation/test';
+import {
+  ApplicationProviderMock,
+  assertStatusForInput,
+  assertValueForInput,
+} from '@/presentation/test';
 import {NavigationStub} from '@/presentation/test/mock-navigation';
 import {
+  cleanup,
   fireEvent,
   render,
   RenderAPI,
   waitFor,
 } from '@testing-library/react-native';
-import {TopNavigationAction} from '@ui-kitten/components';
+import {Icon, TopNavigationAction} from '@ui-kitten/components';
 import React from 'react';
 import {SignUp} from '..';
 
@@ -29,9 +34,65 @@ const makeSut = (): SutTypes => {
 };
 
 describe('SignUp Page', () => {
+  afterEach(cleanup);
+
   test('Should start with initial state', () => {
     const {sut} = makeSut();
     expect(sut.getByTestId('signup_header')).toBeDefined();
+    assertStatusForInput(sut, 'name_input');
+    assertStatusForInput(sut, 'email_input');
+    assertStatusForInput(sut, 'password_input');
+    assertStatusForInput(sut, 'password_confirmation_input');
+
+    assertValueForInput(sut, 'name_input', '');
+    assertValueForInput(sut, 'email_input', '');
+    assertValueForInput(sut, 'password_input', '');
+    assertValueForInput(sut, 'password_confirmation_input', '');
+    expect(sut.getByTestId('password_input')).toHaveProp(
+      'secureTextEntry',
+      true,
+    );
+    expect(sut.getByTestId('password_confirmation_input')).toHaveProp(
+      'secureTextEntry',
+      true,
+    );
+    expect(sut.getByTestId('signup_button')).not.toBeDisabled();
+  });
+
+  test('Should change password visibility on password visibility icon tap', async () => {
+    const {sut} = makeSut();
+    await waitFor(() => {
+      fireEvent.press(
+        sut.getByTestId('password_input_container').findAllByType(Icon)[1],
+      );
+    });
+    expect(sut.getByTestId('password_input')).toHaveProp(
+      'secureTextEntry',
+      false,
+    );
+    expect(
+      sut.getByTestId('password_input_container').findAllByType(Icon)[1],
+    ).toHaveProp('name', 'eye');
+  });
+
+  test('Should change password visibility on password confirmation visibility icon tap', async () => {
+    const {sut} = makeSut();
+    await waitFor(() => {
+      fireEvent.press(
+        sut
+          .getByTestId('password_confirmation_input_container')
+          .findAllByType(Icon)[1],
+      );
+    });
+    expect(sut.getByTestId('password_confirmation_input')).toHaveProp(
+      'secureTextEntry',
+      false,
+    );
+    expect(
+      sut
+        .getByTestId('password_confirmation_input_container')
+        .findAllByType(Icon)[1],
+    ).toHaveProp('name', 'eye');
   });
 
   test('Should call navigation goback on SignOut button press ', async () => {
